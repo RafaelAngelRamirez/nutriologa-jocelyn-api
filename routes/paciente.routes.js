6225b42a2781d7b86df8f62caceeae3d28a29ec5
Rafael Ramirez <rafa.yael@gmail.com>
2021-02-15 17:38:07 -0600

    Eliminar y mofidicar antropometricos

--- routes/paciente.routes.js
+++ routes/paciente.routes.js
@@ -2,7 +2,6 @@ const app = require("express")()
 const Paciente = require("../models/paciente.model")
 
 app.post("/", (req, res, next) => {
-  console.log(req.body)
   delete req.body.datosAntropometricos
 
   new Paciente(req.body)
@@ -14,21 +13,57 @@ app.post("/", (req, res, next) => {
 app.put("/", (req, res, next) => {
   Paciente.findById(req.body._id)
     .exec()
-    .then(p => {
-      if (!p) throw "No existe el id"
+    .then(paciente => {
+      if (!paciente) throw "No existe el id"
       delete req.body._id
-      Object.assign(p, req.body)
-      return p.save()
+
+      Object.assign(paciente, req.body)
+
+      const limpiar = arreglo => {
+        if (!arreglo) return
+        while (arreglo.length > 0) arreglo.pop()
+      }
+      const asignar = (objetivo, nuevo) => nuevo.forEach(x => objetivo.push(x))
+
+      const remplazarArreglo = (objetivo, nuevo) => {
+        if (!nuevo || !objetivo) return
+        limpiar(objetivo)
+        asignar(objetivo, nuevo)
+      }
+      // Asignar arrays.
+      remplazarArreglo(paciente.metasDelPaciente, req.body.metasDelPaciente)
+
+      const padres = ["r24h", "tiemposDeComida"]
+
+      padres.forEach(padre => {
+        const propiedades = [
+          "desayuno",
+          "colacionM",
+          "comida",
+          "colacionV",
+          "cena",
+        ]
+
+        propiedades.forEach(propiedad => {
+          remplazarArreglo(
+            paciente.habitosAlimentarios?.[padre]?.[propiedad],
+            req.body.habitosAlimentarios?.[padre]?.[propiedad]
+          )
+        })
+      })
+
+      return paciente.save()
     })
     .then(p => res.send(p))
     .catch(_ => next(_))
 })
 
-app.put("/datos-antropometricos", (req, res, next) => {
-  Paciente.findById(req.body.id)
+app.put("/datos-antropometricos/crear", (req, res, next) =>
+{
+  Paciente.findById(req.body.idPaciente)
     .exec()
     .then(p => {
-      if (p) throw "No existe el id"
+      if (!p) throw "No existe el id"
       delete req.body._id
       p.datosAntropometricos.push(req.body)
       return p.save()
@@ -36,6 +71,48 @@ app.put("/datos-antropometricos", (req, res, next) => {
     .then(p => res.send(p))
     .catch(_ => next(_))
 })
+
+app.put("/datos-antropometricos/modificar", (req, res, next) => {
+  Paciente.findById(req.body.idPaciente)
+    .exec()
+    .then(p => {
+      if (!p) throw "No existe el id"
+
+      const dato = p.datosAntropometricos.id(req.body._id)
+      if (!dato) throw "No existe el registro antropometrico"
+
+      Object.assign(dato, req.body)
+
+      return p.save()
+    })
+    .then(p => res.send(p))
+    .catch(_ => next(_))
+})
+
+app.delete(
+  "/datos-antropometricos/:idPaciente/:idDatoAntro",
+  (req, res, next) => {
+    let eliminado = null
+    Paciente.findById(req.params.idPaciente)
+      .exec()
+      .then(async paciente => {
+        if (!paciente) throw "No existe el id"
+
+        const eliminado = paciente.datosAntropometricos.pull(
+          req.params.idDatoAntro
+        )
+
+        try {
+          await paciente.save()
+        } catch (error) {
+          return next(err)
+        }
+
+        return res.send(eliminado)
+      })
+      .catch(_ => next(_))
+  }
+)
 
 app.delete("/id/:id", (req, res) => {
   Paciente.findById(req.params.id)
