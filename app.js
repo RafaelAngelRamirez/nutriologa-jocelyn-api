3ebaf0ab8c3db019dd69ef1d2a267a300d5e579d
Rafael Ramirez <rafa.yael@gmail.com>
2021-02-15 19:27:52 -0600

    FIX error de dedo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const port = prcess.env.PORT ?? 3000
+const port = process.env.PORT ?? 3000
 const bodyParser = require("body-parser")
 const autoParser = require("express-query-auto-parse")
 const routes = require("./routes/routes")
